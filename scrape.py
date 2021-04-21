f259d1573005ae24894a08d4c1c9b4719ba44114
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 20:33:49 -0300

    feat: filter usernames that dont follow back

--- scrape.py
+++ scrape.py
@@ -17,3 +17,6 @@ following = list(
 
 followers = list(
     map(lambda x: x.username, list(profile.get_followers())))
+
+## used reversed to list from olders to newers
+dont_follow_back = list(reversed(list(filter(lambda username: username not in followers, following))))
