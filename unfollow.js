d7aaded7264130f49822c627c1226ec1b12c8617
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 21:02:17 -0300

    feat: handle login on instagram

--- unfollow.js
+++ unfollow.js
@@ -8,5 +8,19 @@ const account = require("./env.json");
   const page = await browser.newPage();
   await page.goto("https://instagram.com");
 
+  await Promise.all([
+    page.waitForSelector('[name="username"]'),
+    page.waitForSelector('[name="password"]'),
+  ]);
+
+  await page.type('[name="username"]', account.user);
+  await page.type('[name="password"]', account.password);
+
+  await Promise.all([
+    page.waitForSelector(".sqdOP.L3NKy.y3zKF"),
+    page.click(".sqdOP.L3NKy.y3zKF"),
+  ]);
+
+
   await browser.close();
 })();
