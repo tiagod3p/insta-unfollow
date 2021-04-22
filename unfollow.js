32a6699501a17b05e8aed9c7b3348b203665f2b7
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 21:06:33 -0300

    feat: add unfollow user logic

--- unfollow.js
+++ unfollow.js
@@ -21,6 +21,35 @@ const account = require("./env.json");
     page.click(".sqdOP.L3NKy.y3zKF"),
   ]);
 
+  await Promise.all([page.waitForSelector(".eyXLr.wUAXj")]);
+
+  let count = 1;
+  const QUANTITY_OF_USERS_TO_UNFOLLOW = usersToUnfollow.length
+  for (username of usersToUnfollow) {
+    try {
+
+      await page.goto(`https://instagram.com/${username}`);
+      await Promise.all([
+        page.waitForSelector("._5f5mN.-fzfL._6VtSN.yZn4P"),
+        page.click("._5f5mN.-fzfL._6VtSN.yZn4P"),
+      ]);
+
+      await Promise.all([
+        page.waitForSelector(".aOOlW.-Cab_"),
+        page.click(".aOOlW.-Cab_"),
+      ]);
+
+      console.log(`
+      Unfollowing ${username}.
+      WAITING ${time}ms to proceed to next unfollow.
+      ${count} unfollows.
+      ${QUANTITY_OF_USERS_TO_UNFOLLOW - count} remaining.`);
+
+      count++;
+    } catch (err) {
+      console.log(err.message || err);
+    }
+  }
 
   await browser.close();
 })();
